5ed30943ed30fbaf2075fcafc70e2152aa4a485a
Ryanlydz <48400088+Ryanlydz@users.noreply.github.com>
2021-04-21 18:27:51 -0400

    Add notifications + unregister method

--- app/src/main/java/com/example/ssdproject/DetectedActivityReceiver.java
+++ app/src/main/java/com/example/ssdproject/DetectedActivityReceiver.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
 import com.example.ssdproject.BuildConfig;
 import com.google.android.gms.location.ActivityTransitionEvent;
 import com.google.android.gms.location.ActivityTransitionResult;
@@ -13,6 +16,7 @@ import com.google.android.gms.location.DetectedActivity;
 public class DetectedActivityReceiver extends BroadcastReceiver{
 
     static final String RECEIVER_ACTION = BuildConfig.APPLICATION_ID + ".com.example.ssdproject.DetectedActivityReceiver";
+    private static final String CHANNEL_ID = "activityNotifier" ;
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -25,11 +29,23 @@ public class DetectedActivityReceiver extends BroadcastReceiver{
         if (ActivityTransitionResult.hasResult(intent)) {
 
             ActivityTransitionResult result = ActivityTransitionResult.extractResult(intent);
-
+            
             for (ActivityTransitionEvent event : result.getTransitionEvents()) {
                 int activity = event.getActivityType();
                 int transition = event.getTransitionType();
                 String message = "Transition: " + activity + " " + transition;
+
+                // Send notification
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.strength)
+                        .setContentTitle("It's time to Move!")
+                        .setContentText("Do 20 jumping jacks!")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+                // Display notification
+                notificationManager.notify(1, builder.build());
             }
         }
     }
--- app/src/main/java/com/example/ssdproject/MainActivity.java
+++ app/src/main/java/com/example/ssdproject/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.ssdproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -19,12 +24,41 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String CHANNEL_ID = "1";
+    DetectedActivityReceiver receiver;
+    PendingIntent pendingIntent;
+
+    public void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        createNotificationChannel();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -52,13 +86,12 @@ public class MainActivity extends AppCompatActivity {
 
         // Create ActivityTransitionRequest object
         ActivityTransitionRequest request = new ActivityTransitionRequest(transitions);
-        Context context = getApplicationContext();
 
         Intent intent = new Intent(DetectedActivityReceiver.RECEIVER_ACTION);
 
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent,0);
+        pendingIntent = PendingIntent.getBroadcast(this, 0, intent,0);
 
-          DetectedActivityReceiver receiver = new DetectedActivityReceiver();
+         receiver = new DetectedActivityReceiver();
 
         LocalBroadcastManager.getInstance(this).registerReceiver(receiver, new IntentFilter(DetectedActivityReceiver.RECEIVER_ACTION));
 
@@ -82,5 +115,51 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+
+        // Set up notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.run)
+                .setContentTitle("It's time to Move!")
+                .setContentText("Do 20 jumping jacks!")
+                .setPriority(NotificationCompat.PRIORITY_MAX);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        // Set up timer
+        TimerTask timerTask = new TimerTask() {
+            public void run() {
+                // Display notification
+                notificationManager.notify(1, builder.build());
+                System.out.println("Notification should have displayed!");
+            }
+        };
+        Timer timer = new Timer("Activity Timer");
+        long delay = 2000;
+        timer.schedule(timerTask, delay);
+    }
+
+    private void stopActivityRecognition(){
+
+        Task<Void> task = ActivityRecognition.getClient(this)
+                .removeActivityTransitionUpdates(pendingIntent);
+
+        task.addOnSuccessListener(
+                new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void result) {
+                        pendingIntent.cancel();
+                        Log.d("ActivityRecognition","Transitions API successfully unregistered");
+                    }
+                }
+        );
+
+        task.addOnFailureListener(
+                new OnFailureListener() {
+                    @Override
+                    public void onFailure(Exception e) {
+                        Log.e("ActivityRegister", e.getMessage());
+                    }
+                }
+        );
+
     }
 }
